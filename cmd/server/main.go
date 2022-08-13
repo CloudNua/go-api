a86f258d819ec9a00e5331ed1e0dfe2760737a72
Enda Kelly <kelly.enda@gmail.com>
2022-08-13 15:32:07 +0100

    added: database connection logic

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,9 +1,24 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+
+	"github.com/CloudNua/go-api/internal/db"
+)
 
 func Run() error {
 	fmt.Println("Starting up....")
+
+	db, err := db.NewDatabase()
+	if err != nil {
+		fmt.Println("Failed to connect to db")
+		return err
+	}
+	if err := db.Ping(context.Background()); err != nil {
+		return err
+	}
+	fmt.Println("Successfully connected to db")
 	return nil
 }
 
